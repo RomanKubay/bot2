6bc4ca101aa1113557db6ed099836a89dddb8843
RomanKubay <romankubaj40@gmail.com>
2023-01-30 17:07:10 +0200

    modified:   config.py

--- config.py
+++ config.py
@@ -1,10 +1,9 @@
-# import os
-# API_KEY = os.environ['API_KEY']
-# MONGODB_HOST = os.environ['MONGODB_HOST']
+import os
+API_KEY = os.environ['API_KEY']
+MONGODB_HOST = os.environ['MONGODB_HOST']
 
 #TODO: Перед тим як закинути в GitHub, треба видалити це
-API_KEY = "5881866940:AAFL0u0KAhyz_AilFcUZrRu3KzYzBDWY6G0"
-MONGODB_HOST = "mongodb+srv://RomanKubay:7iunrPEfsDm8UAS8@cluster.myvigzq.mongodb.net/?retryWrites=true&w=majority"
+
 
 DEV_TG_NICKNAME = "thisRomana"
 max_history_leng = 40
